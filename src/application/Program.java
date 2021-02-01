479082d7f04eca0054dcd0dcc0b4c18005296c11
Paulo Souza <pauloogsouza@gmail.com>
2021-02-01 19:26:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
+        Department obj = new Department(1, "Books");
+        Seller sl = new Seller(1, "Joao", "joao@gmail.com", new Date(), 2000.0, obj);
+        System.out.println(sl);
 
     }
 }
